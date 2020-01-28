81eac4814b3e52bed60a4d9b8930867d686acd76
njm1128 <njm1128@naver.com>
2020-01-29 02:10:24 +0900

    AsyncStorage function create

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -4,9 +4,9 @@
  * CREATE DATE : 2020-01-03
  * CREATOR : YUN SANGHO
  *
- * UPDATE DATE :
- * UPDATOR :
- * UPDATE DESCRIPTION :
+ * UPDATE DATE : 2020-01-28
+ * UPDATOR : NOH JAEMIN
+ * UPDATE DESCRIPTION : AsyncStorage function create
  *
  * UPDATE DATE :
  * UPDATOR :
@@ -25,7 +25,7 @@
  * UPDATE DESCRIPTION :
  */
 
-import { Platform } from "react-native";
+import { Platform, AsyncStorage } from "react-native";
 
 /**
  * FUNC NAME : getPlatform
@@ -39,4 +39,70 @@ const getPlatform = () => {
   return currentOS;
 };
 
-export { getPlatform };
+/**
+ * FUNC NAME : setItem
+ * DESCRIPTION : set AsyncStorage Item.
+ * CREATEOR : NOH JAEMIN
+ * TEST : COMPLATE
+ */
+const setItem = (key, value) => {
+  AsyncStorage.setItem(key, value, error => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+
+/**
+ * FUNC NAME : getItem
+ * DESCRIPTION : get AsyncStorage Item.
+ * CREATEOR : NOH JAEMIN
+ * TEST : COMPLATE
+ */
+const getItem = async key => {
+  const data = await AsyncStorage.getItem(key, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  }).then(value => {
+    return value;
+  });
+  return data;
+};
+
+/**
+ * FUNC NAME : modifyItem
+ * DESCRIPTION : modify AsyncStorage Item.
+ * CREATEOR : NOH JAEMIN
+ * TEST : COMPLATE
+ */
+const mergeItem = (key, value) => {
+  AsyncStorage.mergeItem(key, value, error => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+
+/**
+ * FUNC NAME : removeItem
+ * DESCRIPTION : remove AsyncStorage Item.
+ * CREATEOR : NOH JAEMIN
+ * TEST : COMPLATE
+ */
+const removeItem = key => {
+  AsyncStorage.removeItem(key, error => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+
+const Storage = {
+  setItem,
+  getItem,
+  mergeItem,
+  removeItem
+};
+
+export { getPlatform, Storage as AsyncStorage };
